ef9b2ee94c85e2dd07a994ed1097c1727ef4cbe5
kimbos <kimbos0523@gmail.com>
2023-03-05 15:23:52 -0500

    a6 fix the default screen of tuiter and homescreen right nav

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,6 +4,7 @@ import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 import {Routes, Route} from "react-router";
 import HomeComponent from "./home";
+import PostSummaryList from "./post-summary-list";
 
 
 function Tuiter() {
@@ -20,7 +21,10 @@ function Tuiter() {
                 </Routes>
             </div>
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                <WhoToFollowList/>
+                <Routes>
+                    <Route path="home" element={<PostSummaryList/>}/>
+                    <Route path="explore" element={<WhoToFollowList/>}/>
+                </Routes>
             </div>
         </div>
     );
